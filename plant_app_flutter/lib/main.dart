e7f54765ac9606496c68a5c5b5c76be44bc1ab1e
alexdev34 <alexandresch01@hotmail.com>
2024-01-31 12:19:30 +0100

    Ajout de route fonctionnelle pour user

--- plant_app_flutter/lib/main.dart
+++ plant_app_flutter/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app_flutter/user_page.dart';
 import 'login_page.dart';
 import 'register_page.dart';
 import 'acceuil.dart';
-import 'annoucement.dart'; // Importez le fichier contenant la classe Announcement
+import 'annoucement.dart';
+import 'product_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +21,23 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: AuthenticationPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => AuthenticationPage(),
+        '/home': (context) => MyHomePage(),
+        '/signup': (context) => SignupPage(),
+        '/login': (context) => LoginPage(),
+        '/user': (context) => UserPage(), // Replace with your user page
+        '/product': (context) => ProductPage(
+          title: "title",
+          location: "location",
+          price: "price",
+          description: "description",
+          ownerName: "ownerName",
+          ownerImage: "ownerImage",
+          ownerRating: 4,
+        ),
+      },
     );
   }
 }
@@ -28,19 +46,34 @@ class AuthenticationPage extends StatefulWidget {
   @override
   _AuthenticationPageState createState() => _AuthenticationPageState();
 }
-
 class _AuthenticationPageState extends State<AuthenticationPage> {
   int _currentPageIndex = 0;
 
   final List<Widget> _pages = [
     MyHomePage(),
-    SignupPage(), // Ajoutez la page d'inscription
+    SignupPage(),
+    LoginPage(),
+    ProductPage(
+      title: "title",
+      location: "location",
+      price: "price",
+      description: "description",
+      ownerName: "ownerName",
+      ownerImage: "ownerImage",
+      ownerRating: 4,
+    )
   ];
 
   @override
   Widget build(BuildContext context) {
+    Widget currentPage = Container(); // Default value or loading indicator
+
+    if (_currentPageIndex >= 0 && _currentPageIndex < _pages.length) {
+      currentPage = _pages[_currentPageIndex]!;
+    }
+
     return Scaffold(
-      body: _pages[_currentPageIndex],
+      body: currentPage,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentPageIndex,
         onTap: (index) {
@@ -62,3 +95,4 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
     );
   }
 }
+
