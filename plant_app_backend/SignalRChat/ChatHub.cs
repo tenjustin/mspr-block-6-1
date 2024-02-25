29e02d1626421a5302411f8eeb178b1dad314c6a
Justin TENE <tene.justin@gmail.com>
2024-02-25 23:31:00 +0100

    Ajout de la socket coté backend

---
+++ plant_app_backend/SignalRChat/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace plant_app_backend.SignalRChat
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
